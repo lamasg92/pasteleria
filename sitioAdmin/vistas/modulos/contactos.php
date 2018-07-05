1545dc89a6c1cd1df3e1fc6c815c27b61da5e18e
lamasg92 <gabrielca3@gamil.com>
2018-07-05 18:26:02 -0300

    editar contactos

--- sitioAdmin/vistas/modulos/contactos.php
+++ sitioAdmin/vistas/modulos/contactos.php
@@ -15,21 +15,70 @@
   </section>
 
   <section class="content">
-
+    <!--========================Contacto=====================-->
     <div class="box">    
             
       <div class="box-body">
-
+      <h3>Contacto de la Pasteleria</h3> 
+       <?php
         
-      
+        $info = ControladorContacto::ctrMostrarContacto();
+ 
+        echo '<div class="">Dirección:&nbsp;&nbsp;&nbsp'.$info["direccion"].'</div>';  
+
+        echo '<div class="">Teléfono:&nbsp;&nbsp;&nbsp'.$info["telefono"].'</div>';  
+
+        echo '<div class="">Provincia:&nbsp;&nbsp;&nbsp'.$info["provincia"].'</div>'; 
+
+        echo '<div class="">Localidad:&nbsp;&nbsp;&nbsp'.$info["localidad"].'</div>';   
+      ?>
+
+       <div class="box-header with-border text-right"> 
+          <button class="btn btn-primary btnEditarContacto" idContacto="<?php echo $info['id'];?>"  data-toggle="modal" data-target="#modalEditarContacto"> Cambiar información</button>
+
       </div>
+      </div>     
     </div>
+    <!--========================Contacto=====================-->
+
+      <div class="box">
+      <h3>Redes Sociales de la Pasteleria</h3> 
+
+      <table class="table table-striped table-bordered dt-responsive tablaRedesSociales" width="100%">
+
+          <thead>
+            
+            <tr>
+              
+              <th style="width:10px">#</th>
+              <th>Red Social</th>
+              <th>Enlace</th>
+              <th>Estado</th>
+
+            </tr>
+
+          </thead>
+
+      </table>
+       <?php
+        
+        $redes = ControladorRedesSociales::ctrMostrarRedesSociales();
+
+        foreach ($redes as $red) {
+          echo '<div class="box-body">';
+          echo 'Res Social '.$red['id'].'-----Enlace: <a target="_blank" href="'.$red['cuenta'].'">'.$red['nombre'].'</a>-------Estado Actual: '.ucfirst($red['estado']);
+            
+        echo ' <div class="box-header with-border text-right">
+            <button class="btn btn-primary btnEditarRedSocial" idRedSocial="'.$red['id'].'" data-toggle="modal" data-target="#modalEditarRedSocial"> Cambiar información</button>
+        </div></div>';
+        require "contactosModales/redSocial.modal.php";
+        }
+        ?>
+        </div>
+
   </section>
 
   </div>
-
-
-
-
-
-  
+<?php
+require "contactosModales/contacto.modal.php";
+?>  
---
+++ sitioAdmin/vistas/modulos/contactosModales/redSocial.modal.php
@@ -0,0 +1,109 @@
+<!--=====================================
+MODAL CAMBIAR RED SOCIAL
+======================================-->
+
+<div id="modalEditarRedSocial" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+    
+    <div class="modal-content">
+
+      <form method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+          
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          
+          <h4 class="modal-title">Cambiar información de Red Social</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+          
+          <div class="box-body">
+             
+
+            <!--=====================================
+                ENTRADA EDITAR ENLACE RED SOCIAL
+                ======================================-->
+
+                <div class="form-group">
+                  
+                  <h2>Enlace: </h2>
+
+                  <div class="input-group">
+
+                    <input name="id" type="hidden" value="<?php echo $red['id'];?>"> 
+                    <span class="input-group-addon"><i class="fa fa-pencil"></i></span>
+
+                    <input class="form-control input-lg " name="cuenta" type="text" required value="<?php echo $red['cuenta']; ?>"  >
+
+                  </div> 
+
+                </div>
+
+                <!--=====================================
+                ENTRADA EDITAR ESTADO
+                ======================================-->
+
+                <div class="form-group">
+
+                  <h2>Teléfono:</h2>
+                  
+                  <div class="input-group">
+                    
+                    <span class="input-group-addon"><i class="fa fa-pencil"></i></span>
+
+                    <select class="form-control input-lg " name="estado">
+                        <?php if ($red['estado']=='activo') {
+                            echo '<option value="activo" selected>Activo</option>
+                                  <option value="inactivo">Inactivo</option>';
+                                } else {
+                            echo '<option value="activo">Activo</option>
+                                  <option value="inactivo" selected>Inactivo</option>';
+                                }
+                        ?>
+                      </select> 
+
+                  </div> 
+
+                </div>
+          </div>
+        </div>
+
+       <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+      
+
+       
+        <div class="modal-footer">
+          
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+
+        </div>
+
+      </form>
+
+              <?php
+
+                
+                  $editarRedSocial = new ControladorRedesSociales();
+                  $editarRedSocial -> ctrEditarRedSocial();
+
+              ?>
+
+          </div>
+      </div>
+</div>
