b446d4d58963bf43c6d3c54b4a45780c1995ba05
Yanina242 <yaninadaianamartinez@gmail.com>
2018-07-05 18:17:35 -0300

    Vista Contacto

---
+++ sitioSitioWeb/vistas/modulos/contacto.php
@@ -0,0 +1,58 @@
+	<!--feedback-->
+	<section class="contact" id="contact">
+	<div class="container">
+		<div class="heading">
+				<img class="dividerline" src="vistas/img/sep1.png" alt="">
+				<h2>Contactanos</h2>
+				<img class="dividerline" src="vistas/img/sep1.png" alt="">
+		</div>
+	<h3>Información de contacto</h3>
+	<h5>Contactanos o ubicanos en:	</h5>
+		<div class="row">
+			<div class="col-md-6">
+
+				<div class="col-md-4">
+					<img  src="vistas/img/ubicacion.png" alt="80px" width="80px">
+				</div>
+				
+					<h4>B°Santa Rita Mz 52 BL23<br> 
+					Rosario de Lerma <br>
+					SALTA </h4>
+					
+			</div>	
+			<div class="col-md-6">
+				<div class="col-md-3">
+					<img  src="vistas/img/telefono.jpg" alt="80px" width="80px">
+				</div>
+				 <br>
+					<h4>(387)155027931</h4>
+		
+
+			</div>
+		</div>
+	</div>
+	 <div class="container w960">
+	 <h3>Formulario de Contacto</h3>
+	 <h5>Para todo tipo de pregunta de preguntas o consultas completa el siguiente formulario:</h5>
+      <div class="row">
+		<div class="done">
+			<div class="alert alert-success">
+				<button type="button" class="close" data-dismiss="alert">×</button>
+				Your message has been sent. Thank you!
+			</div>
+		</div>
+       <form method="post" action="contact.php" id="contactform">
+          <input name="name" type="text" class="contact col-md-6" placeholder="Tu nombre *" >
+          <input name="email" type="email" class="contact noMarr col-md-6" placeholder="Dirección de e-mail *" >
+          <textarea name="comment" class="contact col-md-12" placeholder="Mensaje *"></textarea>
+          <input type="submit" id="submit" class="contact submit" value="Envar mensaje">
+        </form>
+      </div>
+    </div>
+    <div class="text-center">
+
+				<img  src="vistas/img/facebook.png" alt="50px" width="50px">
+				<img  src="vistas/img/instagram.png" alt="80px" width="80px">
+				<img  src="vistas/img/twitter.png" alt="60px" width="60px">
+	</div>
+	</section>
