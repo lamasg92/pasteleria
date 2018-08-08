5debe19e2abff802eab1b7a5a5e8cd94abf8e397
lamasg92 <gabrielca3@gamil.com>
2018-08-08 13:36:10 -0300

    Modificacion de carrito

--- sitioSitioWeb/index.php
+++ sitioSitioWeb/index.php
@@ -1,13 +1,17 @@
 <?php
 
 require_once "controladores/sitio.controlador.php";
-
+require_once "controladores/productos.controlador.php";
 
 require_once "modelos/sitio.modelo.php";
 require_once "modelos/rutas.php";
+require_once "modelos/producto.modelo.php";
 
 require_once "extensiones/PHPMailer/PHPMailerAutoload.php";
 require_once "extensiones/vendor/autoload.php";
+
+
+
 
 
 $sitio = new ControladorSitio();
