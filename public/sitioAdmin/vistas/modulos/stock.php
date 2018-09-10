f595496104ccadfe06531e07448cc9427e43edda
gyrotech155 <chacon.romina91@gmail.com>
2018-09-10 11:30:55 -0300

    Agregar y Editar Stock

---
+++ public/sitioAdmin/vistas/modulos/stock.php
@@ -0,0 +1,42 @@
+<div class="content-wrapper">
+    
+  <section class="content-header">
+
+    <h2 class="text-center">Stock</h2>
+ 
+    <ol class="breadcrumb">
+
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+
+      <li class="active">Stock</li>
+      
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="box">    
+                 
+        <table class="table table-striped table-bordered dt-responsive tablaStock" width="100%">
+
+          <thead>
+            
+            <tr>
+              <th style="width:10px">#</th>
+              <th>Dia</th>
+              <th>Stock</th>
+              <th>Acción</th>
+            </tr>
+
+          </thead>
+
+        </table>
+    
+    </div>
+  </section>
+  </div>
+<?php
+//require "categoriasModales/crearCategoria.modal.php";
+require "stockModales/editarStock.modal.php";
+?> 
